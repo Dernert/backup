674311059e0a0c30ae04b59492aa7ef2d56ec08f
Dernert <dernert@outlook.com>
2016-10-13 10:29:19 +0200

    Änderungen
    Broadcast wurde hinzugefügt 30min, 15min, 5min, 1min, 30sek, 10sek, 5sek

--- script.sh
+++ script.sh
@@ -11,10 +11,34 @@ minecraft_folder=/home/minecraft/server
 backup_folder=/private-backup
 #AKTUELLES DATUM
 datum=$(date +%d_%m_%y)
-#BACKUP NAMEN
+#BACKUPNAME
 backup_name=serverbackup_full_$datum.tgz
-
+#SERVERNACHRICHT
+server_message_30_min="Der Server wird in 30 Minuten neugestartet!"
+server_message_15_min="Der Server wird in 15 Minuten neugestartet!"
+server_message_5_min="Der Server wird in 5 Minuten neugestartet!"
+server_message_1_min="Der Server wird in 1 Minuten neugestartet!"
+server_message_30_sec="Der Server wird in 30 Sekunden neugestartet!"
+server_message_10_sec="Der Server wird in 10 Sekunden neugestartet!"
+server_message_5_sec="Der Server wird in 5 Sekunden neugestartet!"
+################################################################################
 echo "Script wird gestartet."
+sleep 5s
+
+echo "Servernachricht: t- 30 Minuten"
+tmux send-keys -t $session_name "bc $server_message_30_min" Enter
+sleep 30m
+tmux send-keys -t $session_name "bc $server_message_15_min" Enter
+sleep 15m
+tmux send-keys -t $session_name "bc $server_message_5_min" Enter
+sleep 5m
+tmux send-keys -t $session_name "bc $server_message_1_min" Enter
+sleep 1m
+tmux send-keys -t $session_name "bc $server_message_30_sec" Enter
+sleep 30s
+tmux send-keys -t $session_name "bc $server_message_10_sec" Enter
+sleep 10s
+tmux send-keys -t $session_name "bc $server_message_5_sec" Enter
 sleep 5s
 
 echo "Server wird gestoppt"
@@ -26,11 +50,10 @@ echo "Server ist gestoppt."
 #VERZEICHNIS WIRD GEWECHSELT
 tmux send-keys -t $session_name "cd .." Enter
 sleep 5s
-							
+
 #BACKUP WIRD ERSTELLT ---- SYNTAX: tar -czf Backupname.tgz Ordner_der_gepackt_werden_soll
 tmux send-keys -t $session_name "tar -czf $backup_name $minecraft_folder " Enter
 echo "Backup wird erstellt"
-
 #!!EMPFEHLUNG!!!
 #Zeit hochsetzen, sollte das Packen des Ordner nicht fertig sein und das Script läuft weiter
 #kann es zu Datenverlust kommen!
